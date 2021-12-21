f31248c4ffb712253dd05e4760bdfc84749af902
Keita <fast.break.baske03@gmail.com>
2021-12-22 01:13:57 +0900

    update:FlatListを用いてニュースを一覧で表示

--- App.tsx
+++ App.tsx
@@ -1,37 +1,42 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, SafeAreaView, FlatList, StatusBar } from 'react-native'
 import ListItem from './components/news'
 import articles from './dummies/articles.json'
+import { isAndroid } from './util/app'
 
 type article = {
-  urlToImage: string;
-  title: string;
-  author: string;
+  urlToImage: string
+  title: string
+  author: string
+}
+
+type renderItem = {
+  item: article
 }
 
 export default function App() {
-  const items = articles.map((article: article, index: number) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        key={index}
-      />
-    )
-  })
+  const renderItem = ({ item }: renderItem) => (
+    <ListItem
+      imageUrl={item.urlToImage}
+      title={item.title}
+      author={item.author}
+    />
+  )
 
   return (
-    <View style={styles.container}>
-      {items}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={renderItem}
+        keyExtractor={(_item, index) => index.toString()}
+      />
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingTop: isAndroid() ? StatusBar.currentHeight : 0,
+    backgroundColor: '#fff'
   }
 })
