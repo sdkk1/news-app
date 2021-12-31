aa9dd070fd754134c87a8d84ca127e638e03cd77
Keita <fast.break.baske03@gmail.com>
2021-12-31 21:08:20 +0900

    add: navigationの作成

---
+++ src/navigation/index.tsx
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from '../screens/home'
+
+const Stack = createStackNavigator()
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation
