1c2cb678dd3f21f945c3a1d9f1c25b829760aac2
Keita <fast.break.baske03@gmail.com>
2022-01-01 23:33:57 +0900

    update: 型定義を追加

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,6 +1,17 @@
+import { RouteProp } from '@react-navigation/native'
 import { Article } from "./article"
 
 export type RootStackParamList = {
   Home: undefined
   Article: { article: Article }
+  Clip: undefined
+}
+
+export type BottomTabParamList = {
+  HomeTab: undefined
+  ClipTab: undefined
+}
+
+export type BottomTabRouteProp = {
+  route: RouteProp<BottomTabParamList, keyof BottomTabParamList>
 }
